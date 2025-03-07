7f75307cba4ba6538e0d65e1dfb4e9168cc884cc
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2025-03-07 13:35:41 +0100

    separate standalone and sdk processors

---
+++ partial_span_processor_standalone.py
@@ -0,0 +1,36 @@
+import time
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+  OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+from opentelemetry.sdk._logs._internal.export import SimpleLogRecordProcessor
+from opentelemetry.sdk.trace import TracerProvider
+from partial_span_processor import PartialSpanProcessor
+
+# Create a TracerProvider
+provider = TracerProvider()
+
+# Configure OTLP exporters
+span_exporter = OTLPSpanExporter(endpoint="localhost:4317",
+                                 insecure=True)  # grpc
+log_exporter = OTLPLogExporter(endpoint="http://localhost:4318/v1/logs")  # http
+
+span_processor = PartialSpanProcessor(span_exporter,
+                                      SimpleLogRecordProcessor(log_exporter))
+provider.add_span_processor(span_processor)
+
+# Set the global TracerProvider
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+# Start a span (logs heartbeat and stop events)
+with tracer.start_as_current_span("partial_span_1"):
+  print("partial_span_1 is running")
+  with tracer.start_as_current_span("partial_span_2"):
+    print("partial_span_2 is running")
+    with tracer.start_as_current_span("partial_span_3"):
+      print("partial_span_3 is running")
+      time.sleep(10)
+
+provider.shutdown()
